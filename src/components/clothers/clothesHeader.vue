b13b60f858570dece5a0b02e560ee40b89f2a41e
ibroximov-fx <ibrohimovahmad91@gmail.com>
2025-08-13 05:00:41 -0700

    add clothes responsive

---
+++ src/components/clothers/clothesHeader.vue
@@ -0,0 +1,26 @@
+<template>
+<header class="px-[15px] lg:max-w-[1230px] mx-auto lg:px-[0px]">
+  <div class="pt-10 flex items-center justify-between">
+  <div class="flex items-center gap-9">
+    <img src="../../assets/about.png" alt="about">
+    <nav class=" hidden lg:flex gap-8">
+      <p class="font-medium text-4">Home</p>
+      <p class="font-medium text-4">Collections</p>
+      <p class="font-medium text-4">New</p>
+    </nav>
+  </div>
+    <img class="block lg:hidden" src="../../assets/response.png" alt="response">
+  <div class="flex items-center">
+<img class="hidden lg:block lg:pr-[45px]" src="../../assets/bigLike.svg" alt="bigLike">
+    <div class="hidden lg:block lg:bg-[#000000] rounded-[22px]">
+      <p class="text-[#FFFFFF] py-4 px-[21px] font-medium text-[12px] tracking-[2px]">Cart</p>
+    </div>
+    <img class="w-[41px] h-[41px]  lg:ml-[-10px] w-[50px] h-[50px] " src="../../assets/shop.png" alt="shop">
+    <img class="ml-[6px] w-[41px] h-[41px] lg:ml-[42px] w-[50px] h-[50px]" src="../../assets/profile.png" alt="profile">
+  </div>
+  </div>
+</header>
+</template>
+<script>
+
+</script>
---
+++ src/components/clothers/orderSummary.vue
@@ -0,0 +1,36 @@
+
+<template>
+  <div class="mt-10 border border-[#D9D9D9] lg:border border-[#D9D9D9] pt-3 pb-[37px] w-[306px] lg:mt-[0px]">
+    <div class="pl-10 pr-9">
+      <p class="font-medium text-[14px] pt-[51px]">ORDER SUMMARY</p>
+      <div class="flex justify-between pt-[22px]">
+        <p class="font-medium text-[12px]">Subtotal</p>
+        <p class="font-medium text-[12px]">${{subtotal}}</p>
+      </div>
+      <div class="flex justify-between pt-[5px]">
+        <p class="font-medium text-[12px]">Shipping</p>
+        <p class="font-medium text-[12px]">${{shipping}}</p>
+      </div>
+      <div class="border border-[#D9D9D9] mt-[18px]"></div>
+      <div class="flex justify-between items-center pt-6">
+        <p class="font-medium text-[16px] text-[#1E1E1E] uppercase">
+          Total
+          <span class="font-medium text-[12px] uppercase text-[#0000008C]">(Tax incl.)</span>
+        </p>
+        <p>${{total}}</p>
+      </div>
+      <div class="flex gap-2 pt-9 items-center">
+        <input class="w-[17px] h-[18px] border border-[#8A8A8A]" type="checkbox">
+        <p class="text-[11px]">I agree to the Terms and Conditions</p>
+      </div>
+      <button class="bg-[#D9D9D9] py-[10px] w-full mt-[15px]">Continue</button>
+    </div>
+  </div>
+</template>
+<script setup>
+defineProps({
+  subtotal: Number,
+  shipping: Number,
+  total: Number
+})
+</script>
