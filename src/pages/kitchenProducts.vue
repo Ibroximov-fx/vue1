d1e347fc00f2897c482fb17e9c30fc3ab7563273
ibroximov-fx <ibrohimovahmad91@gmail.com>
2025-08-28 12:15:05 -0700

    charts and sliders push

--- src/pages/kitchenProducts.vue
+++ src/pages/kitchenProducts.vue
@@ -1,8 +1,3 @@
-<script setup>
-
-import Buttons from "../buttons/buttons.vue";
-</script>
-
 <template>
   <main class="min-h-screen w-full
   bg-gradient-to-br from-orange-50 via-white to-green-50
@@ -14,43 +9,78 @@ import Buttons from "../buttons/buttons.vue";
       <RouterLink to="/headlight" class="font-bold text-[20px]">headlight</RouterLink>
       <RouterLink to="/kitchenProduct" class="font-bold text-[20px]">kitchenProducts</RouterLink>
     </div>
-    <buttons>
-      Game out
-    </buttons>
-    <!-- Section 1 -->
-    <section class="w-full max-w-3xl bg-white rounded-3xl shadow-lg p-12 border border-orange-100">
-      <h2 class="text-3xl font-extrabold text-orange-600 mb-4">
-        Meva va Sabzavotlar
-      </h2>
-      <p class="text-gray-600 leading-relaxed text-lg">
-        Yangi uzilgan mevalar va vitaminlarga boy sabzavotlar — sog‘lom hayot uchun.
-      </p>
-    </section>
+    <div class="grid grid-cols-3 gap-10">
+    <div class="max-w-[300px] bg-white rounded-2xl shadow-lg overflow-hidden">
+      <div class="max-w-[400px] ">
+  <swiper :navigation="false" :modules="[Navigation]" class="mySwiper">
+    <swiper-slide><img src="../assets/portfolio/homePhone.png" alt="homePhone"></swiper-slide>
+    <swiper-slide><img src="../assets/portfolio/menuPhone.png" alt="menuPhone"></swiper-slide>
+    <swiper-slide><img src="../assets/portfolio/childrenPage.png" alt="childrenPage"></swiper-slide>
+    <swiper-slide><img src="../assets/portfolio/compPage.png" alt="compPage"></swiper-slide>
+    <swiper-slide><img src="../assets/portfolio/gamesPage.png" alt="gamesPage"></swiper-slide>
+  </swiper>
+      </div>
+  <div class="p-4 space-y-2">
+    <p class="text-lg font-bold text-gray-800">Sayt zakaz qilish</p>
+    <p class="text-gray-600">Landing page tayyorlash</p>
+    <p class="text-gray-600">Internet magazin yaratish</p>
+    <p class="text-gray-600">Korporativ sayt</p>
+    <p class="text-gray-600">SEO optimizatsiya</p>
+  </div>
+    </div>
+    <div class="max-w-[300px] bg-white rounded-2xl shadow-lg overflow-hidden">
+      <div class="max-w-[400px]  h-[251px] ">
+        <swiper
+            :direction="'vertical'"
+            :modules="[Mousewheel, Pagination]"
+            class="mySwiper h-full"
+        >
+          <swiper-slide><img src="../assets/portfolio/homePhone.png" alt="homePhone"></swiper-slide>
+          <swiper-slide><img src="../assets/portfolio/menuPhone.png" alt="menuPhone"></swiper-slide>
+          <swiper-slide><img src="../assets/portfolio/childrenPage.png" alt="childrenPage"></swiper-slide>
+          <swiper-slide><img src="../assets/portfolio/compPage.png" alt="compPage"></swiper-slide>
+          <swiper-slide><img src="../assets/portfolio/gamesPage.png" alt="gamesPage"></swiper-slide>
+        </swiper>
+      </div>
+      <div class="p-4 space-y-2">
+        <p class="text-lg font-bold text-gray-800">Sayt zakaz qilish</p>
+        <p class="text-gray-600">Landing page tayyorlash</p>
+        <p class="text-gray-600">Internet magazin yaratish</p>
+        <p class="text-gray-600">Korporativ sayt</p>
+        <p class="text-gray-600">SEO optimizatsiya</p>
+      </div>
+    </div>
+    </div>
 
-    <!-- Section 2 -->
-    <section class="w-full max-w-3xl bg-white rounded-3xl shadow-lg p-12 border border-green-100">
-      <h2 class="text-3xl font-extrabold text-green-600 mb-4">
-        Oshxona Asboblari
-      </h2>
-      <p class="text-gray-600 leading-relaxed text-lg">
-        Pichoqlar, idishlar, tovoqlar va zamonaviy oshxona texnikalari.
-      </p>
-    </section>
-
-    <!-- Section 3 -->
-    <section class="w-full max-w-3xl bg-white rounded-3xl shadow-lg p-12 border border-red-100">
-      <h2 class="text-3xl font-extrabold text-red-500 mb-4">
-        Maxsus Takliflar
-      </h2>
-      <p class="text-gray-600 leading-relaxed text-lg">
-        Oilaviy to‘plamlar, chegirmalar va aksiyalar — oshxonangiz uchun kerakli hamma narsa.
-      </p>
-    </section>
-
+    <div class="text-center">
+      <p class="font-bold text-[30px]">ZedGroupda kelgan va ZedGroup uquvchilari</p>
+      <canvas id="myChart" class="max-w-[400px] h-[400px]"></canvas>
+    </div>
   </main>
 
 </template>
+<script setup>
+import { onMounted } from "vue"
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import { Navigation, Mousewheel, Pagination } from 'swiper/modules'
 
-<style scoped>
+import Chart from 'chart.js/auto'
 
-</style>
+const config = {
+  type: 'doughnut',
+  data: {
+    labels: ['ZedGroup', 'AlfaGroup'],
+    datasets: [{
+      data: [3, 2],
+    }]
+  },
+}
+
+onMounted(() => {
+  const ctx = document.getElementById("myChart")
+  new Chart(ctx, config)
+})
+</script>
